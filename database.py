e5bdda4e001a2de6cafe613199fd377474bdc37a
dtapia28 <42440487+dtapia28@users.noreply.github.com>
2021-02-16 10:42:37 -0300

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('postgresql://super:J@vXZ5evdnqX3KZ@daniel25109-2061.postgres.pythonanywhere-services.com:12061/voxcall_bdd')
+engine = create_engine('')
 db_session = scoped_session(sessionmaker(autocommit=False,
                                         autoflush=False,
                                         bind=engine))
